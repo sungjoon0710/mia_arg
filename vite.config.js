1030d54f03402061243866b4ac1eabea613f7a96
sungjoon0710 <sungjoonpark0710@gmail.com>
2025-03-12 11:10:24 -0400

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/mia_arg/', // <-- Important for GitHub Pages subpath
   plugins: [react()],
   resolve: {
     alias: {
